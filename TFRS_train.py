24b1a6065324089474c2b19bb4087ebd6f479ef0
wliafe <914099435@qq.com>
2024-11-06 14:03:42 +0800

    refactor(TFRS_train.py): 移除冗余变量并保存模型

--- TFRS_train.py
+++ TFRS_train.py
@@ -4,5 +4,5 @@ from TFRS import net, train_iter, test_iter, device, model_path
 
 if __name__ == '__main__':
     lr, num_epochs = 0.01, 3
-    train_loss, train_acc, test_acc = ML.image_classification_train(net, train_iter, test_iter, num_epochs, lr, device)
+    ML.image_classification_train(net, train_iter, test_iter, num_epochs, lr, device)
     torch.save(net.state_dict(), model_path)
