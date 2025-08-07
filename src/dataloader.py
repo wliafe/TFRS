a5bfb134f6bbfc05cc9dd21969a74934ca7d0695
wliafe <914099435@qq.com>
2025-08-07 12:17:23 +0800

    更新TFRS项目

--- src/dataloader.py
+++
@@ -1,8 +0,0 @@
-import mltools
-from torchvision import datasets
-
-
-def tfrs(transform, path="../data", batch_size=8, num_workers=8, pin_memory=True, drop_last=True):
-    data = datasets.ImageFolder(f"{path}/TFRS", transform=transform)
-    train_data, val_data, test_data = mltools.split_data(data, [0.7, 0.15, 0.15])
-    return mltools.iter_data([train_data, val_data, test_data], batch_size, num_workers=num_workers, pin_memory=pin_memory, drop_last=drop_last)
